35ee96d9c74c58bcefa539238a32d876207216cd
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-10-09 18:51:39 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const authRoutes  = require("./routes/Auth");
 const shipmentRoutes = require("./routes/Shipment") ;
-const { connectDB } = require("./config/database")
+const { connectDB } = require("./config/database");
+
 require("dotenv").config();
 
 const app = express();
@@ -9,12 +10,10 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 8000;
 
+connectDB()
 app.listen(PORT , () => {
     console.log(`App is listening on PORT ${PORT}`);
 })
-
-connectDB();
-
 
 app.use("/api/v1/auth" , authRoutes)
 app.use("/api/v1/shipment" , shipmentRoutes)
